ea09e1c1253f4ddff66f6656a22bc16d753351cd
Johannes Binder <binder-johannes@web.de>
2023-03-23 12:08:59 +0100

    Added code to read plaintext and key, shift all Latin letters forward and print the result

--- Main.java
+++ Main.java
@@ -1,7 +1,28 @@
 package encryptdecrypt;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dv ulfmw z givzhfiv!");
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        int key = sc.nextInt();
+
+        for (int i = 0; i < text.length(); i++) {
+            int charAsInt = text.charAt(i);
+            if (charAsInt >= 'a' && charAsInt <= 'z') {
+                charAsInt = charAsInt - 'a';
+                charAsInt = (charAsInt + key) % 26;
+                charAsInt = charAsInt + 'a';
+                System.out.print((char) charAsInt);
+            } else if (charAsInt >= 'A' && charAsInt <= 'Z') {
+                charAsInt = charAsInt - 'A';
+                charAsInt = (charAsInt + key) % 26;
+                charAsInt = charAsInt + 'A';
+                System.out.print((char) charAsInt);
+            } else {
+                System.out.print((char) charAsInt);
+            }
+        }
+
     }
 }
