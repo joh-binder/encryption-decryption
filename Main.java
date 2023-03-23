1648d2c607da33fa098128e8d5e690f87d3c294b
Johannes Binder <binder-johannes@web.de>
2023-03-23 12:09:55 +0100

    Split code into two different functions for encryption and decryption

--- Main.java
+++ Main.java
@@ -2,26 +2,33 @@ package encryptdecrypt;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void encrypt(String input, int shift) {
+        for (int i = 0; i < input.length(); i++) {
+            int charAsInt = input.charAt(i);
+            charAsInt = charAsInt + shift;
+            System.out.print((char) charAsInt);
+        }
+    }
+
+    public static void decrypt(String input, int shift) {
+        for (int i = 0; i < input.length(); i++) {
+            int charAsInt = input.charAt(i);
+            charAsInt = charAsInt - shift;
+            System.out.print((char) charAsInt);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String encOrDec = sc.nextLine();
         String text = sc.nextLine();
         int key = sc.nextInt();
 
-        for (int i = 0; i < text.length(); i++) {
-            int charAsInt = text.charAt(i);
-            if (charAsInt >= 'a' && charAsInt <= 'z') {
-                charAsInt = charAsInt - 'a';
-                charAsInt = (charAsInt + key) % 26;
-                charAsInt = charAsInt + 'a';
-                System.out.print((char) charAsInt);
-            } else if (charAsInt >= 'A' && charAsInt <= 'Z') {
-                charAsInt = charAsInt - 'A';
-                charAsInt = (charAsInt + key) % 26;
-                charAsInt = charAsInt + 'A';
-                System.out.print((char) charAsInt);
-            } else {
-                System.out.print((char) charAsInt);
-            }
+        if (encOrDec.equalsIgnoreCase("enc")) {
+            encrypt(text, key);
+        } else if (encOrDec.equalsIgnoreCase("dec")) {
+            decrypt(text, key);
         }
 
     }
